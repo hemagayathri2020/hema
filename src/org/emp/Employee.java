84070bde91adf0c5ada04fd38072d81542e8e9ad
hemagayathri2020 <hemagayathribalakrishnan@gmail.com>
2021-05-06 20:27:13 +0530

    message

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,44 @@
+package org.emp;
+
+public class Employee {
+	
+private void empId() {
+System.out.println("Employee id is 090697");
+}
+private void empName() {
+	// TODO Auto-generated method stub
+	System.out.println("Employee name is hema ");
+
+}
+private void empDob() {
+	// TODO Auto-generated method stub
+	System.out.println("Employee dob is june 9");
+
+}
+private void empPhone() {
+	// TODO Auto-generated method stub
+	System.out.println("Employee phone numberis 9944578972 ");
+
+}
+private void empMail() {
+	// TODO Auto-generated method stub
+	System.out.println("Employee mail id is hemagayathri97@gmail.com");
+
+}
+private void empAddress() {
+	// TODO Auto-generated method stub
+	System.out.println("Employee address is chrompet");
+
+}
+public static void main(String[] args) {
+	Employee e=new Employee();
+	e.empId();
+	e.empName();
+	e.empDob();
+	e.empPhone();
+	e.empMail();
+	e.empAddress();
+}
+
+
+}
